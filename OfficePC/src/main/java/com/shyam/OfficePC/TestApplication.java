2bf55db2fb8bb823491372445d42abae54e77edf
shyam-p8 <117067690+shyam-p8@users.noreply.github.com>
2022-12-22 22:59:33 -0800

    first commit

---
+++ OfficePC/src/main/java/com/shyam/OfficePC/TestApplication.java
@@ -0,0 +1,16 @@
+package com.shyam.OfficePC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+		
+		
+		System.out.println("test program main class runnuing fine.");
+	}
+
+}
---
+++ OfficePC/src/main/java/com/shyam/OfficePC/WebController.java
@@ -0,0 +1,22 @@
+package com.shyam.OfficePC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class WebController {
+	
+	
+	
+	@GetMapping("/")
+	public String home()
+	{
+		return "hello user, your first web application running fine.";
+		
+	}
+	
+	
+
+}
